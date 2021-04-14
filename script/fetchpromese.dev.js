02627119b823521d0127ddbb67e91d80f6e4e92c
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-04-14 14:09:16 -0400

    Incializa el proy
    Se crea js con llamada a api

---
+++ script/fetchpromese.dev.js
@@ -0,0 +1,10 @@
+const getuser = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        });
+
+}
+
+getuser()
