1652f639d97d84216171a60464ac26e31fb4a105
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-04-14 14:29:47 -0400

    async await

---
+++ script/fetchAsync.dev.js
@@ -0,0 +1,10 @@
+const getuser = async() => {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/users ')
+
+    const data = await response.json()
+    console.log(data)
+
+}
+
+getuser()
