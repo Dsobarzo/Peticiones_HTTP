596648b067deae33a5a73bf4907510886ada6dd1
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-04-14 15:08:00 -0400

    Update AxiosAsync.dev.js

--- script/AxiosAsync.dev.js
+++ script/AxiosAsync.dev.js
@@ -2,11 +2,18 @@
     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
     console.log(response);
 }*/
+const username = document.querySelector("#name")
+const mail = document.querySelector("#mail")
+const web = document.querySelector("#web")
 
 //destructurado 
 const getuser = async() => {
     const { data: datos } = await axios.get('https://jsonplaceholder.typicode.com/users');
     console.log(datos);
+
+    username.innerHTML = datos[0].name;
+    mail.innerHTML = datos[0].email;
+    web.innerHTML = datos[0].website;
 }
 
 getuser();
